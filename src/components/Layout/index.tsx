f48f810f574bb7ed68487c7b2355fc8a7a5835f8
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-01 22:33:52 +0100

    feat: add sidebar component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container, Wrapper } from './styles'
 import Main from '../Main/index'
 import MenuBar from '../MenuBar'
+import SideBar from '../SideBar'
 
 const Layout: React.FC = () => {
   return (
@@ -9,7 +10,7 @@ const Layout: React.FC = () => {
      <Wrapper>
        <MenuBar />
        <Main />
-       {/* <Sidebar /> */}
+       <SideBar />
      </Wrapper>
    </Container>
   )
